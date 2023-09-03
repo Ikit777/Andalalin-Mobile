d2a541897821597b396cbd6a37aed2dacc44298a
ikit777 <faisalmtp24@gmail.com>
2023-09-03 22:39:28 +0800

    init

--- app.config.js
+++ app.config.js
@@ -8,5 +8,22 @@ module.exports = ({ config }) => {
       appStatus: process.env.APP_STATUS,
       apiURL: process.env.API_URL,
     },
+    android: {
+      package: "com.andalalin",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON, //"./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./app/assets/image/icon.png",
+        backgroundColor: "#ffffff",
+      },
+      permissions: [
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE",
+        "android.permission.DOWNLOAD_WITHOUT_NOTIFICATION",
+        "android.permission.ACCESS_NETWORK_STATE",
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.FOREGROUND_SERVICE",
+      ],
+    },
   };
 };
