47d0ca93dd61f4f6bd1681227e15f2fd6d3fa45c
ikit777 <faisalmtp24@gmail.com>
2023-09-03 20:42:28 +0800

    init

--- app.config.js
+++ app.config.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 
-export default {
-    expo: {
-        extra: {
-            appStatus: process.env.APP_STATUS,
-            apiURL: process.env.API_URL
-        }
-    }
-}
+export default ({ config }) => {
+  return {
+    extra: {
+      eas: {
+        projectId: "2b044a40-5746-4ede-8008-ca66914cc9c6",
+      },
+      appStatus: process.env.APP_STATUS,
+      apiURL: process.env.API_URL,
+    },
+    ...config,
+  };
+};
